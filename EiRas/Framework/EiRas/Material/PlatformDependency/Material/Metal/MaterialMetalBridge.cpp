193dcb2e0446351fd7f5eacce8edf0cdd890711d
MonsterENT <monster1057981979@outlook.com>
2019-11-13 23:39:18 +0800

    -

---
+++ EiRas/Framework/EiRas/Material/PlatformDependency/Material/Metal/MaterialMetalBridge.cpp
@@ -0,0 +1,24 @@
+//
+//  MaterialMetalBridge.cpp
+//  EiRasMetalBuild
+//
+//  Created by MonsterENT on 11/13/19.
+//  Copyright © 2019 MonsterENT. All rights reserved.
+//
+
+#include "MaterialMetalBridge.hpp"
+#include <Graphics/GraphicsRenderState.hpp>
+#include "MaterialMetalAdapter.hpp"
+
+using Graphics::GraphicsRenderState;
+using MaterialSys::MaterialMetalBridge;
+
+MaterialMetalBridge::MaterialMetalBridge(std::string name)
+{
+    raw_obj = createMaterialMetal(name);
+}
+
+void MaterialMetalBridge::UpdateRenderState(Graphics::GraphicsRenderState* renderState, Shader* shaderObj)
+{
+    UpdateRenderStateMetal(raw_obj, renderState, shaderObj);
+}
