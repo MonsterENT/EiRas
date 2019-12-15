819694efb8ae77f83aa86a89a02d527da84ef959
MonsterENT <monster1057981979@outlook.com>
2019-12-15 12:03:38 +0800

    1

--- EiRas/Framework/EiRas/Global/PlatformDefine.h
+++ EiRas/Framework/EiRas/Global/PlatformDefine.h
@@ -1,8 +1,8 @@
 #ifndef PlatformDefine_hpp
 #define PlatformDefine_hpp
 
-#define GRAPHICS_DX 1
-#define GRAPHICS_METAL 0
+#define GRAPHICS_DX 0
+#define GRAPHICS_METAL 1
 
 #if GRAPHICS_METAL
 typedef unsigned int        UINT;
