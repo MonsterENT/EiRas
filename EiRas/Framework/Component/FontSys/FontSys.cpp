906c5d99c93a02887f954fb8da73e8cb51b22f05
Monster <monster1057981979@outlook.com>
2020-05-01 02:08:01 +0800

    Update

--- EiRas/Framework/Component/FontSys/FontSys.cpp
+++ EiRas/Framework/Component/FontSys/FontSys.cpp
@@ -5,6 +5,8 @@
 
 #pragma comment( lib,"freetype.lib" )
 
+using std::map;
+using std::pair;
 using namespace FontSys;
 
 //void scanToBuffer(unsigned char* dstBuffer, int dstBufferWidth, int dstBufferHeight, FT_Bitmap bitmap, int x, int y)
@@ -68,14 +70,25 @@ Text* Font::GetText(std::string text, int fontSizeInPixel)
 
     for (int i = 0; i < lens; i++)
     {
+        char t = c_str_text[i];
+
+        map<char, CharData>::iterator charDataIt = _CachedCharData.find(t);
+        if (charDataIt != _CachedCharData.end())
+        {
+            textObj->_AddFontData(&(charDataIt->second));
+            continue;
+        }
+
         FT_Set_Transform(face, 0, &vector);
-        ft_error = FT_Load_Char(face, c_str_text[i], FT_LOAD_RENDER);
+        ft_error = FT_Load_Char(face, t, FT_LOAD_RENDER);
         FT_Bitmap bitmap = face->glyph->bitmap;
 
         Math::rect_float outUVRect;
         _uint fontMapIndex = FontManager::SharedInstance()->_StoreFontData(bitmap.buffer, bitmap.width, bitmap.rows, face->glyph->bitmap_left, face->glyph->bitmap_top, outUVRect);
 
-        textObj->_AddFontData(fontMapIndex, outUVRect);
+        CharData data = CharData(outUVRect, fontMapIndex);
+        _CachedCharData.insert(pair<char, CharData>(t, data));
+        textObj->_AddFontData(&data);
 
         vector.x += face->glyph->advance.x;
         vector.y += face->glyph->advance.y;
