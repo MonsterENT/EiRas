e486d6607b12ab017f55eb2f419fb3208e8c5254
MonsterEN <monster1057981979@outlook.com>
2021-03-09 10:36:38 +0800

    1

--- EiRas/SampleBuild/Common/Resource/Shader/DX/GP.hlsl
+++ EiRas/SampleBuild/Common/Resource/Shader/DX/GP.hlsl
@@ -5,9 +5,13 @@ SamplerState _DefaultSampler : register(s0);
 [numthreads(1, 1, 1)]
 void CSMain(uint3 groupIdx : SV_GroupID)
 {
-    if(groupIdx.x == 2)
+    if(groupIdx.x == 3)
     {
-        _CommonBuffer[groupIdx.x] = 1;
+        _CommonBuffer[groupIdx.x] = _CommonBuffer[3] + 1;
+    }
+    else if(groupIdx.x == 2)
+    {
+        _CommonBuffer[groupIdx.x] = _CommonBuffer[2] + 1;
     }
     else
     {
